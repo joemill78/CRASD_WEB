263e6cbaae9748d6469e86eff13c212950e4f5a4
joemill78 <activatedrr@gmail.com>
2018-11-14 11:33:27 -0600

    adjusted edit device info. included name, phone and added download chart info report

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
 from __future__ import division
-from flask import Flask, render_template, request, redirect, url_for, flash, Markup, session
+from flask import Flask, render_template, request, redirect, url_for, flash, Markup, session, Response
 from database import DB
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, NumberRange, Optional, Length, IPAddress
-#from wtforms_components import IntegerField
 from password_manager import PW_MANAGER
 app = Flask(__name__)
 app.secret_key ='s3cr3t'
@@ -15,9 +14,11 @@ class ValidateMonDevice(FlaskForm):
     ip = StringField("ip", validators=[DataRequired(), IPAddress()])
     name = StringField("name", validators=[DataRequired(), Length(max=30)])
 
+
 class ValidateEditMonDevice(FlaskForm):
     ip = StringField("ip", validators=[DataRequired(), IPAddress()])
     name = StringField("name", validators=[DataRequired(), Length(max=30)])
+
 
 class ValidateDeviceRecords(FlaskForm):
     ip = StringField("ip", validators=[DataRequired(), IPAddress()])
@@ -29,10 +30,14 @@ class ValidateDeviceRecords(FlaskForm):
     phone = StringField("phone", validators=[DataRequired(), Length(max=12)])
     download = StringField("download", validators=[DataRequired(), Length(max=4)])
     upload = StringField("upload", validators=[DataRequired(), Length(max=4)])
+    name = StringField("name", validators=[DataRequired(), Length(max=20)])
 
 
 def mem_to_meg(value):
-    return round(int(value) / 1024)
+    if value:
+        return round(int(value) / 1024)
+    else:
+        return 0
 
 
 def split_line(line):
@@ -162,7 +167,12 @@ def edit_device_info():
                 if form.zip.data != output['zip']:
                     device['zip'] = form.zip.data
                     items_updated += ' Zip Code '
-
+                if form.phone.data != output['phone']:
+                    device['phone'] = form.phone.data
+                    items_updated += ' Phone '
+                if form.name.data != output['name']:
+                    device['name'] = form.name.data
+                    items_updated += ' Name '
                 if device:
                     pg.update_device(device) # we send data to postgres here
                     flash("Device Info Updated: " + items_updated)
@@ -311,7 +321,11 @@ def charts():
         values2.append(line[0])
         labels2.append(line[1].strftime('%Y-%m-%d %H:%M:%S'))
 
-    legend = 'Memory Used (Memory Available {0}) Megs'.format(mem_to_meg(datums['mem']))
+    if datums:
+        legend = 'Memory Used (Memory Available {0}) Megs'.format(mem_to_meg(datums['mem']))
+    else:
+        legend = 'Memory Used (0) Megs'
+
     legend2 = "Load Average"
 
     return render_template('charts.html', output=output, datums=datums, mem_to_meg=mem_to_meg,
@@ -338,6 +352,101 @@ def user_login():
 def logout():
     session['logged_in'] = False
     return dashboard()
+
+
+@app.route("/get_csv_device")
+def get_csv_device():
+    pg = DB()
+    pg.connect()
+    output = pg.get_device_info()
+    datums = pg.get_device_datums()
+    pg.close()
+    title = 'Device Information\n'
+    headers = 'Server ID,Status,IP Address,Address,City,State,Zip,Country,Download Rate,Upload Rate\n'
+    data = '{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n'.format(output['device_id'], output['status'], output['ip'],
+                                                              output['address'], output['city'], output['state'],
+                                                              output['zip'], output['country'], output['download_rate'],
+                                                              output['upload_rate'])
+
+    title2 = '\n\nSystem Information\n'
+    headers2 = 'OS,Kernel,CPU Vendor,CPU Model,RAM (Gigs)\n'
+    data2 = '{0},{1},{2},{3},{4}\n'.format(datums['os'], datums['kernel'], datums['cpu_vendor'], datums['cpu_model'],
+                                           mem_to_g(datums['mem']))
+
+    csv = title + headers + data + title2 + headers2 + data2
+    return Response(csv, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=device_report.csv"})
+
+
+@app.route("/get_csv_events")
+def get_csv_events():
+    pg = DB()
+    pg.connect()
+    available_mon_events = pg.get_available_mon_events()
+    active_mon_events = pg.get_active_mon_events()
+    active_reachability_events = pg.get_active_reachability_events()
+    historical_mon_events = pg.get_all_h_mon_events()
+    historical_reachability_events = pg.get_all_h_reach_events()
+    pg.close()
+    title = 'Available Monitoring Events\n'
+    headers = 'Event Type ID,Event Description,Monitor\n'
+    data = ""
+    for line in available_mon_events:
+        data += '{0},{1},{2}\n'.format(line['name'], line['description'], line['monitor'])
+
+    title2 = '\n\nActive Monitoring Events\n'
+    headers2 = 'Event ID,Event Type,Resource,Start Time\n'
+    data2 = ""
+    for line in active_mon_events:
+        data2 += '{0},{1},{2},{3}\n'.format(line['event_id'], line['name'], line['identifier'],
+                                            line['start_time'].strftime('%Y-%m-%d %H:%M:%S'))
+
+    title3 = '\n\nActive Reachability Events\n'
+    headers3 = 'Event ID,Device Name,IP Address,Start Time\n'
+    data3 = ''
+    for line in active_reachability_events:
+        data3 += '{0},{1},{2},{3}\n'.format(line['event_id'], line['name'], line['ip'],
+                                            line['start_time'].strftime('%Y-%m-%d %H:%M:%S'))
+
+    title4 = '\n\nHistorical Monitoring Events\n'
+    headers4 = 'Event ID,Event Type,Resource,Start Time,Stop Time\n'
+    data4 = ''
+    for line in historical_mon_events:
+        data4 += '{0},{1},{2},{3},{4}\n'.format(line[0], line[1], line[2], line[3].strftime('%Y-%m-%d %H:%M:%S'),
+                                                line[4].strftime('%Y-%m-%d %H:%M:%S'))
+
+    title5 = '\n\nHistorical Reachability Events\n'
+    headers5 = 'Event ID,Device Name,IP Address,Start Time,Stop Time\n'
+    data5 = ''
+    for line in historical_reachability_events:
+        data5 += '{0},{1},{2},{3},{4}\n'.format(line[0], line[1], line[2], line[3].strftime('%Y-%m-%d %H:%M:%S'),
+                                                line[4].strftime('%Y-%m-%d %H:%M:%S'))
+
+    csv = (title + headers + data + title2 + headers2 + data2 + title3 + headers3 + data3 + title4 + headers4 + data4 +
+           title5 + headers5 + data5)
+    return Response(csv, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=events_report.csv"})
+
+
+@app.route("/get_csv_charts")
+def get_csv_charts():
+    pg = DB()
+    pg.connect()
+    data_memory = pg.get_mem_utilized()
+    load_avg = pg.get_load_avg()
+    pg.close()
+    title = 'Memory Utilization\n'
+    headers = 'Value,Time\n'
+    data = ""
+    for line in data_memory:
+        data += '{0},{1}\n'.format(line[0], line[1])
+
+    title2 = '\n\nLoad Average\n'
+    headers2 = 'Value,Time\n'
+    data2 = ""
+    for line in load_avg:
+        data2 += '{0},{1}\n'.format(line[0], line[1])
+
+    csv = (title + headers + data + title2 + headers2 + data2)
+    return Response(csv, mimetype="text/csv", headers={"Content-disposition": "attachment; filename=charts_report.csv"})
 
 
 if __name__ == "__main__":
